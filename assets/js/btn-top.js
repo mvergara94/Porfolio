09d9f42e9442b420b1a2df9678c521f8f6190aaf
mvergara94 <mariovergaralorena@gmail.com>
2023-05-23 14:14:25 -0300

    Arrumando comportamento do botão de voltar ao topo

--- assets/js/btn-top.js
+++ assets/js/btn-top.js
@@ -1,17 +1,22 @@
 const botaoTopo = document.querySelector('#back-to-top');
 
-window.addEventListener('scroll', () => {
+function atualizarBotaoTopo() {
   const posicao = window.scrollY + window.innerHeight;
   const metadePagina = document.body.offsetHeight / 2;
 
-  if (screen.width > 820) {
+  if (window.innerWidth > 820) {
     if (posicao > metadePagina) {
       botaoTopo.style.display = 'block';
     } else {
       botaoTopo.style.display = 'none';
     }
+  } else {
+    botaoTopo.style.display = 'none';
   }
-});
+}
+
+window.addEventListener('scroll', atualizarBotaoTopo);
+window.addEventListener('resize', atualizarBotaoTopo);
 
 function subirTopo() {
   botaoTopo.addEventListener('click', () => {
